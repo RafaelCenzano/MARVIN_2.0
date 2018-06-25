9a48824d1b4272beeb85e68b9370a5a80b6230e6
Savagecoder77 <savagecoder77@gmail.com>
2018-06-24 21:03:57 -0700

    new folder to hold files need to setup Marvin after download

---
+++ Setup_Files/setup.py
@@ -0,0 +1,16 @@
+import os
+import platform
+
+print ('Decting Operating System')
+print ('Your os name is ' + os.name)
+print ('Your os type is ' + platform.system())
+print ('Your os version is ' + platform.release())
+
+if platform.system() == 'Linux':
+    print ('Linux')
+elif platform.system() == 'Darwin':
+    print ('Mac')
+elif platform.system() == 'Windows':
+    print ('Windows')
+else:
+    print ('We dont have a way to set up Marvin on your Operating System. \nIf this is a mistake make sure to report it as an issue at https://github.com/SavageCoder77/MARVIN_2.0')
---
+++ Setup_Files/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+touch ~/login.json
+touch ~/data.json

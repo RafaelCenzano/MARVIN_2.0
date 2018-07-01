3749e686f169432c88bb4548c7d8bcb9528c3622
Savagecoder77 <savagecoder77@gmail.com>
2018-06-30 17:43:07 -0700

    fixed class to pass to standby with external function

--- Marvin_Commands.py
+++ Marvin_Commands.py
@@ -8,8 +8,6 @@ import subprocess
 import webbrowser
 #smtplib for sending emails
 import smtplib
-#re for searching in commands for arguments
-import re
 
 def speak(spokenString):
     print(spokenString)
@@ -22,19 +20,26 @@ def speak(spokenString):
 
 def listen():
     r = sr.Recognizer()
+    #using microphone to detect audio
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration = )
+        #adjust for ambiet sounds
+        r.adjust_for_ambient_noise(source, duration = 1)
+        #listen
         audio = r.listen(source)
     data = ""
     try:
+      #recognize with google's speech recognition
       data = r.recognize_google(audio)
       print("You said: " + data)
     except sr.UnknownValueError:
-      print("I didnt get that")
+      #when google api didn't understand audio
+      print("I didn\'t get that")
     except sr.RequestError as e:
+      #when connection or Api offline
       print("Api or connection is not working.\n The error is {0}".format(e))
     return data
 
+class MarvinCommands(Exception): pass
 def commands(command):
 
     if 'open reddit' in command:
@@ -49,11 +54,10 @@ def commands(command):
 
     if 'standby' in command:
         speak('Going on standby')
+        raise MarvinCommands
 
     if "where is" in command:
         location = command.split(" ")[2:].join(" ")
-        #location = command
-
         speak("Hold on, I will show you where " + location + " is.")
         url = ("https://www.google.nl/maps/place/" + location + "/&amp;")
         webbrowser.open(url, new = 2)

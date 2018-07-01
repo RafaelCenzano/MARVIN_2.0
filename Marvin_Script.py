01d50ce846c4625149918d37ade3ebe5165de361
Savagecoder77 <savagecoder77@gmail.com>
2018-06-30 21:04:13 -0700

    added chat commands

--- Marvin_Script.py
+++ Marvin_Script.py
@@ -15,7 +15,7 @@ while True :
         try:
             while 1:
                 print('listening for commands')
-                data = essentials.listen() #use listen function in Marvin_Commands.py
+                data = essentials.listen() #use listen function in commands.py
                 commands.dataCommands(data.lower()) # check for command and lower what was just said
         except commands.MarvinCommands: # except and pass to resume stanby
             pass
@@ -23,20 +23,31 @@ while True :
         essentials.speak("exiting")
         exit()
     else:
-        essentials.speak("would you like to be on standby?")
-        standby = raw_input(": ")
-        if standby == "yes":
-            essentials.speak("Type start to repoen voice commands or quit to exit")
-            y_n = raw_input(": ")
-            if y_n == "start":
+        essentials.speak("Would you like to do chat commands?")
+        chat_input = raw_input(': ')
+        if chat_input == 'yes':
+            try:
+                while 1:
+                    print('Awaiting commands')
+                    data  = raw_input(': ')
+                    commands.dataCommands(data.lower()) # check for command and lower what was just said
+            except commands.MarvinCommands: # except and pass to resume stanby
+                pass
+        else:
+            essentials.speak("Would you like to be on standby?")
+            standby = raw_input(": ")
+            if standby == "yes":
+                essentials.speak("Type start to repoen voice commands or quit to exit")
+                y_n = raw_input(": ")
+                if y_n == "start":
             # NOTE: !important! READ THIS
             # -> exit all the if statements which will bring us back to the beginning of the loop where it will ask 'would you like to do voice commands'
             # by running the 'continue' function you will be able to skip down to the bottom of the while loop and since there is nothing there to run it will loop back to the beginning
-                continue
-            else:
+                    continue
+                else:
+                    essentials.speak("exiting")
+                    exit() # exit the while loop to end the code session
+            else: # user doesn't want to be on standby so end code session
                 essentials.speak("exiting")
-                exit() # exit the while loop to end the code session
-        else: # user doesn't want to be on standby so end code session
-            essentials.speak("exiting")
-            exit() # exit code session
+                exit() # exit code session
     # end of core loop

021c69035445ed64f6019a17976a56c8f33956b4
Savagecoder77 <savagecoder77@gmail.com>
2018-07-09 20:45:38 -0700

    finished send email command

--- marvin/commands.py
+++ marvin/commands.py
@@ -1,10 +1,9 @@
 #Imports
-import os # to open files
-import webbrowser # webbrowser to open websites
-import smtplib # smtplib for sending emails
+from webbrowser import open as webopen # webbrowser to open websites
 import essentials # import speak and listen
 from calculator import calculator # import calculator code
-import webscrape
+import webscrape # import webscrape functions
+from json import load # import json load
 
 
 #####################
@@ -19,10 +18,10 @@ def dataCommands(command):
 
     if 'open reddit' in command:
         subreddit = command.split(" ")[2:] # split for anything after 'open reddit'
-        subreddit_joined = (" ").join(subreddit) # joining anything that was split from after 'where is'
+        subreddit_joined = (" ").join(subreddit) # joining anything that was split from after 'open reddit'
         essentials.speak('Opening subreddit ' + subreddit_joined) # saying the subreddit page
         url = ('https://www.reddit.com/r/' + subreddit_joined) # url with reddit page
-        webbrowser.open(url, new = 2) # open url in browser
+        webopen(url, new = 2) # open url in browser
         print('Done!')
 
     elif 'standby' in command:
@@ -32,35 +31,57 @@ def dataCommands(command):
     elif 'youtube' in command:
         video = command.split(" ")[1:] # split for anything after 'youtube'
         video_joined = (" ").join(video) # joining anything that was split from after 'youtube'
-        essentials.speak('Opening first video for ' + video_joined)
+        essentials.speak('Opening first video for ' + video_joined + ' on YouTube')
         webscrape.scrapeYoutube(video_joined)
-'''
-    elif 'amazon' in command:
-        amazon = command.split(" ")[1:]
-        amazon_search = (" ").join(amazon)
-        essentials.speak('Searching amazon for ' + amazon_search)
-        url = ('https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + amazon_search)
-        webbrowser.open(url, new = 2)
-        url2 = str(webscrape.scrapeAmazon(command))
-        print url2
-        webbrowser.open(url2, new = 2)
-        print('Done!')'''
 
     elif 'where is' in command:
         location = command.split(" ")[2:] # split for anything after 'where is'
         location_joined = (" ").join(location) # joining anything that was split from after 'where is'
         essentials.speak('Hold on, I will show you where ' + location_joined + ' is.') # saying the location heard
         url = ('https://www.google.nl/maps/place/' + location_joined + '/&amp;') # url with location
-        webbrowser.open(url, new = 2) # open url in browser
+        webopen(url, new = 2) # open url in browser
         print('Done!')
 
-    elif 'exit' in command or 'quit' in command or 'leave' in command:
+    elif command == 'exit' or command == 'quit' or command == 'leave':
         essentials.speak('exiting')
         exit() # leave program
 
-    elif 'open calculator' in command or 'run calculator' in command or 'calculator' in command:
+    elif 'open calculator' in command or 'run calculator' in command or command == 'calculator':
         calculator() # run calculator code from calculator.py
 
-    elif 'hello' in command or 'hi' in command:
+    elif command == 'hello' or command == 'hi':
         essentials.speak('Hello')
 
+    elif command == 'send email':
+        try:
+            with open('marvin/json/data.json', 'r') as listen_data:
+                listen_chat_data = load(listen_data)
+            if listen_chat_data['listen'] == 0:
+                essentials.speak('Who would you like to send this email to?')
+                email_recipent = raw_input(': ').lower()
+                essentials.speak('What is the subject of the email?')
+                email_subject = raw_input(': ')
+                essentials.speak('What is the message you would like to send to ' + email_recipent)
+                email_body = raw_input(': ')
+                essentials.email(email_recipent, email_subject, email_body)
+            elif listen_chat_data['listen'] == 1:
+                essentials.speak('Who would you like to send this email to?')
+                email_recipent = essentials.listen().lower()
+                essentials.speak('What is the subject of the email?')
+                email_subject = essentials.listen()
+                essentials.speak('What is the message you would like to send to ' + email_recipent)
+                email_body = essentials.listen()
+                essentials.email(email_recipent, email_subject, email_body)
+            else:
+                essentials.speak('A file is missing data report to issues on github')
+                pass
+        except essentials.MarvinEmail:
+            pass # stop command
+
+    elif 'amazon' in command:
+        amazon = command.split(" ")[1:]
+        amazon_search = (" ").join(amazon)
+        essentials.speak('Searching amazon for ' + amazon_search)
+        url = ('https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + amazon_search)
+        webopen(url, new = 2)
+        print('Done!')
--- marvin/essentials.py
+++ marvin/essentials.py
@@ -1,32 +1,70 @@
-#Imports
-import speech_recognition as sr # speech_recognition to turn speech to string
-from gtts import gTTS # gtts for text to speech
-import subprocess # subprocess for playing audio
-
-
 ################################
 # File for essential functions #
 ################################
 
 
 def speak(spokenString):
+    #Imports
+    from gtts import gTTS # gtts for text to speech
+    from subprocess import Popen, PIPE # subprocess for playing audio
+    #Code
     print(spokenString)
     tts = gTTS(text = spokenString, lang = 'en-uk') # create string into mp3 file using gtts
     tts.save('Marvin_Speak.mp3')
-    proc = subprocess.Popen(['mpg321 Marvin_Speak.mp3'], stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+    proc = Popen(['mpg321 Marvin_Speak.mp3'], stdout = PIPE, stderr = PIPE, shell = True)
     (out, err) = proc.communicate() # opening speak file
 
 def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source: # using microphone to detect audio
-        r.adjust_for_ambient_noise(source, duration = 1) #adjust for ambiet sounds for 1 second
+    #Imports
+    from speech_recognition import Recognizer, Microphone, UnknownValueError, RequestError # speech_recognition to turn speech to string
+    #Code
+    r = Recognizer()
+    with Microphone() as source: # using microphone to detect audio
+        r.adjust_for_ambient_noise(source, duration = 0.5) #adjust for ambiet sounds for 1 second
         audio = r.listen(source) # listen for audio
     data = ''
     try:
         data = r.recognize_google(audio) # recognize with google's speech recognition
         print('You said: ' + data)
-    except sr.UnknownValueError:
+    except UnknownValueError:
         print('I didn\'t get that') # when google api didn't understand audio
-    except sr.RequestError as e:
+    except RequestError as e:
         print('Api or connection is not working.\n The error is {0}'.format(e)) # when connection or Api offline
     return data
+
+class MarvinEmail(Exception): pass
+def email(recipient, subject, email_message):
+    #import
+    from smtplib import SMTP
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    from json import load
+
+    with open('../pass.json', 'r') as email_user_data:
+        data = load(email_user_data)
+        email_user = data['email_address']
+        email_pass = data['email_password']
+
+    with open('marvin/json/contacts.json', 'r') as contacts_open:
+        contact_data = load(contacts_open)
+    if recipient in contact_data['contacts']:
+        recipient_email = contact_data['contacts'][recipient]['email']
+    else:
+        speak(recipient + ' is not in our contacts use the "add contacts" command to add them')
+        raise MarvinEmail
+
+    #message area
+    msg = MIMEMultipart()
+    msg['From'] = 'Marvin'
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(email_message,'plain'))
+    text = msg.as_string()
+
+    #sending code
+    server = SMTP('smtp.gmail.com', 587) # connection to 587 port for gmail
+    server.starttls() #start connection
+    server.login(email_user, email_pass) # login with credentials
+    server.sendmail(email_user, recipient_email, text) # send email
+    server.quit() # quit connection
+    print('Done!') # done

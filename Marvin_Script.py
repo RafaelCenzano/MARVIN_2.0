fc4feb71bd724870181ac35c3a4b4613c7b51bb3
Savagecoder77 <savagecoder77@gmail.com>
2018-07-15 10:08:34 -0700

    added login system

--- Marvin_Script.py
+++ Marvin_Script.py
@@ -1,15 +1,44 @@
 # Imports
 from marvin import commands
 from marvin import essentials
+from json import load
+from codecs import encode
+from hashlib import sha512
 
+
+# LOGIN
+with open('marvin/json/pass.json', 'r') as login_data:
+    new_login_data = load(login_data)
+search_login = new_login_data['logins']
+while True:
+    while True:
+        print('\n\n\n\n##### LOGIN #####\n')
+        print('Login in with:')
+        for x in search_login:
+            print(x)
+        login_usr = raw_input('>')
+        if login_usr not in search_login:
+            print('\n##############\nIncorrect User\n##############\n')
+        else:
+            break
+    print('Please Type Username')
+    login_pass = raw_input('>')
+    new_login = encode(login_pass, 'rot13')
+    print('Please Type Password')
+    login_pass = raw_input('>')
+    login_pass2 = sha512(login_pass + new_login).hexdigest()
+    if login_pass2 == new_login_data['logins'][login_usr]['pass'] and new_login == new_login_data['logins'][login_usr]['usr']:
+        break
+    else:
+        print('\n#####################\nIncorrect Credentials\n#####################\n')
 # MAIN
-while True :
+while True:
     # core loop this is the part that will be looped to check user wants to keep using voice commands
     # it will always ask the user the prompt below when the while loop goes back to the start
     # part that will run when while loop resets
     essentials.speak('Would you like to do voice commands, chat commands, or be on standby?')
     print(' \nOPTIONS:\n1. voice commands\n2. chat commands\n3. standby\n4. quit\n ')
-    beg_input = raw_input(": ")
+    beg_input = raw_input(">")
     # end of part that will run when while loop resets
  
     if beg_input == '1' or 'voice' in beg_input: # once recording data is done and marvin completed commands it will restart the loop and ask 'would you like to do voice commands'
@@ -24,7 +53,7 @@ while True :
         try:
             while 1:
                 print('Awaiting commands')
-                data  = raw_input('')
+                data = raw_input('')
                 commands.dataCommands(data.lower(), 0) # check for command and lower what was just said and adds 0 value to show raw input and not talking commands
         except commands.MarvinCommands: # except and pass to resume stanby
             pass #restart loop

6acc0d28746d46946425a59944e5d9ce35fb25af
Savagecoder77 <savagecoder77@gmail.com>
2018-07-15 18:40:20 -0700

    imported functions that were needed

--- setup.py
+++ setup.py
@@ -1,42 +1,44 @@
 # Imports
-import os
-import platform
-import json
-import hashlib
+from os import system, name
+from platform import system, release
+from json import dump
+from hashlib import sha512
 
 # Main Code
 while True:
-    print('Setting Up Marvin\nPlease set a password for the ADMIN acount\nMake the password longer than 5 characters')
+    print('Setting Up Marvin\nADMIN acount username is ADMIN\nPlease set a password for the ADMIN acount\nMake the password longer than 5 characters')
     new_pass = raw_input('>')
     if len(new_pass) <= 5:
         print('Make your password longer than 5 characters please')
     else:
-        pass_new = hashlib.sha512(new_pass + 'NQZVA').hexdigest()
+        pass_new = sha512(new_pass + 'NQZVA').hexdigest()
         break
 
-check_os = platform.system()
+check_os = system()
 print ('Decting Operating System')
-print ('Your os name is ' + os.name)
+print ('Your os name is ' + name)
 print ('Your os type is ' + check_os)
-print ('Your os version is ' + platform.release())
+print ('Your os version is ' + release())
 
 with open('marvin/json/contacts.json', 'w') as outfile1:
     var = {'contacts':{'example':{"email":"email@domain.com","number":"number here"}}}
-    json.dump(var, outfile1)
+    dump(var, outfile1)
 with open('marvin/json/pass.json', 'w') as outfile2:
     var1 = {'logins':{'ADMIN':{"usr":"NQZVA","pass":pass_new}}}
-    json.dump(var1, outfile2)
-
-os.system('pip install -r requirements.txt')
+    dump(var1, outfile2)
 
 if check_os == 'Linux':
     print('Going to install tkinter for GUI')
-    os.system('sudo apt-get install python-tk')
+    system('sudo apt-get install python-tk')
 elif check_os == 'Darwin':
     print('We need to install Homebrew so that we can install portaudio')
-    os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+    system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
     print('Installing portaudio')
-    os.system('brew install portaudio')
+    system('brew install portaudio')
 elif check_os == 'Windows': pass
 else:
     print ('We dont have a way to set up Marvin on your Operating System.\nIf this is a mistake make sure to report it as an issue at https://github.com/SavageCoder77/MARVIN_2.0')
+    exit()
+
+system('pip install -r requirements.txt')
+print('\n\nCompleted all downloads')

bead6d430b714680f25481cec7696ec0b9f9e4e4
Savagecoder77 <savagecoder77@gmail.com>
2018-07-19 21:26:09 -0700

    optimized paths and created new file to hold OS information in case its needed later and marvin version

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Imports
-from os import system, name
+import os
 from platform import system, release
 from json import dump
 from hashlib import sha512
@@ -15,30 +15,53 @@ while True:
         break
 
 check_os = system()
+os_release = release()
 print ('Decting Operating System')
-print ('Your os name is ' + name)
 print ('Your os type is ' + check_os)
-print ('Your os version is ' + release())
+print ('Your os version is ' + os_release)
 
-with open('marvin/json/contacts.json', 'w') as outfile1:
-    var = {'contacts':{'example':{"email":"email@domain.com","number":"number here"}}}
-    dump(var, outfile1)
-with open('marvin/json/pass.json', 'w') as outfile2:
-    var1 = {'logins':{'ADMIN':{"pass":pass_new}}}
-    dump(var1, outfile2)
+print('Creating needed files')
+
+contacts_path = os.path.join('marvin','json','contacts.join')
+pass_path = os.path.join('marvin','json','pass.join')
+
+try:
+    with open('Os.json', 'w') as outfile:
+        var2 = {"Marvin_Release":"0.0.1","Os_data":{"OS":check_os,"os_release":os_release}}
+        dump(var2, outfile)
+    with open(contacts_path, 'w') as outfile1:
+        var = {"contacts":{"example":{"email":"email@domain.com","number":"number here"}}}
+        dump(var, outfile1)
+    with open(pass_path, 'w') as outfile2:
+        var1 = {"logins":{"ADMIN":{"pass":pass_new}}}
+        dump(var1, outfile2)
+except Exception as e:
+    print('We ran into a problem\nPlease report this issue ' + str(e))
+    exit()
 
 if check_os == 'Linux':
-    print('Going to install tkinter for GUI')
-    system('sudo apt-get install python-tk')
+    try:
+        print('\nGoing to install tkinter for GUI')
+        os.system('sudo apt-get install python-tk')
+    except Exception as e:
+        print('We ran into a problem\nPlease report this issue ' + str(e))
+        exit()
 elif check_os == 'Darwin':
-    print('We need to install Homebrew so that we can install portaudio')
-    system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
-    print('Installing portaudio')
-    system('brew install portaudio')
+    try:
+        print('We need to install Homebrew so that we can install portaudio')
+        os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+        print('Installing portaudio')
+        os.system('brew install portaudio')
+    except Exception as e:
+        print('We ran into a problem\nPlease report this issue ' + str(e))
+        exit()
 elif check_os == 'Windows': pass
 else:
     print ('We dont have a way to set up Marvin on your Operating System.\nIf this is a mistake make sure to report it as an issue at https://github.com/SavageCoder77/MARVIN_2.0')
     exit()
-
-system('pip install -r requirements.txt')
-print('\n\nCompleted all downloads')
+try:
+    system('pip install -r requirements.txt')
+    print('\n\nAll files and installs completed\nYou can now run Marvin with Marvin_Script.py')
+except Exception as e:
+    print('We ran into a problem\nPlease report this issue ' + str(e))
+    exit()

731c2f679b6a50fbdbec01a24e599c7c15adacfe
Savagecoder77 <savagecoder77@gmail.com>
2018-07-21 13:38:24 -0700

    fixed imports for better efficency

--- marvin/essentials.py
+++ marvin/essentials.py
@@ -1,13 +1,18 @@
+# Imports
+from smtplib import SMTP # smtplib for connection and sending of email
+from email.mime.text import MIMEText # MIMEText for formatting
+from email.mime.multipart import MIMEMultipart # MIMEMultipart changing sender
+from json import load # to open contacts.json to see contacts to be able to send email
+from gtts import gTTS # gtts for text to speech
+from subprocess import Popen, PIPE # subprocess for playing audio
+from speech_recognition import Recognizer, Microphone, UnknownValueError, RequestError # speech_recognition to turn speech to string
+
 ################################
 # File for essential functions #
 ################################
 
 class MarvinEssentials(Exception): pass
 def speak(spokenString):
-    #Imports
-    from gtts import gTTS # gtts for text to speech
-    from subprocess import Popen, PIPE # subprocess for playing audio
-    #Code
     print(spokenString)
     tts = gTTS(text = spokenString, lang = 'en-uk') # create string into mp3 file using gtts
     tts.save('Marvin_Speak.mp3')
@@ -15,9 +20,6 @@ def speak(spokenString):
     (out, err) = proc.communicate() # opening speak file
 
 def listen():
-    #Imports
-    from speech_recognition import Recognizer, Microphone, UnknownValueError, RequestError # speech_recognition to turn speech to string
-    #Code
     r = Recognizer()
     with Microphone() as source: # using microphone to detect audio
         r.adjust_for_ambient_noise(source, duration = 0.5) #adjust for ambiet sounds for 1 second
@@ -44,84 +46,7 @@ def commandInput(type_of_input):
     else:
         print('Fatal Error create Issue on Github')
 
-def ADMIN(contact_path, pass_path):
-    from json import load, dump
-    from codecs import encode
-    from hashlib import sha512
-    from os import system
-    while True:
-        print('\nOnly use ADMIN acount for administrative tasks')
-        print('\n######## ADMIN MENU ########\n\n1. Create New User\n2. Update Marvin\n3. Leave ADMIN Menu')
-        ADMIN_input = raw_input('>')
-        if ADMIN_input == '1' or 'user' in ADMIN_input.lower():
-            print('You will choose a username and password for the new user\nType a User')
-            new_user = raw_input('>')
-            with open(contact_path, 'r') as login_data:
-                new_user_data = load(login_data)
-            search_login = new_user_data['logins']
-            if new_user in search_login:
-                print('This user exists already')
-            else:
-                while True:
-                    print('\nType a password for the new user thats over 5 characters')
-                    new_user_pass = raw_input('>')
-                    if len(new_user_pass) <= 5:
-                        print('Make your password over 5 characters please')
-                        pass
-                    else:
-                        break
-                new_user_pass_encrypted = sha512(new_user_pass + new_user).hexdigest()
-                print('Creating User')
-                with open(pass_path, 'w') as outfile:
-                    new_user_data['logins'][new_user] = {"pass":new_user_pass_encrypted}
-                    dump(new_user_data, outfile)
-                print('New user created')
-        elif ADMIN_input == '2' or 'update' in ADMIN_input.lower():
-            ('Checking for Update')
-            system('git pull')
-        elif ADMIN_input == '3' or 'exit' in ADMIN_inputor or 'leave' in ADMIN_input or 'quit' in ADMIN_input:
-            ('Exiting ADMIN MENU')
-            break
-
-import time
-def listofcontacts(contact_list):
-    time.sleep(0.7)
-    for c in contact_list:
-        c_letters = list(c)
-        c_letter_first = c_letters[0]
-        c_letters_rest = c_letters[1:]
-        c_letters_rest_joined = ("").join(c_letters_rest)
-        c_letter_first_upper = str(c_letter_first.upper())
-        print(c_letter_first_upper + c_letters_rest_joined)
-
-def contactList(contact_path):
-    from threading import Thread
-    from json import load
-    with open(contact_path, 'r') as contact_data_list:
-        list_contact_data = load(contact_data_list)
-        contact_list = list_contact_data['contacts']
-    if not list_contact_data['contacts']:
-        print('No contacts use the add contacts command to add some')
-        raise MarvinEssentials
-    elif not list_contact_data:
-        print('Fatal Error\nMissing data make sure that you ran setup.py before running this script')
-        raise MarvinEssentials
-    else:
-        thread_list_contact = Thread(target = listofcontacts, args = (contact_list,))
-        thread_list_contact.start()
-        speak('Opening contact list for you now')
-
-def openCalculator():
-    from os import system
-    system('python2.7 marvin/calculator.py')
-
 def email(recipient, subject, email_message, pass_path, contact_path):
-    #import
-    from smtplib import SMTP # smtplib for connection and sending of email
-    from email.mime.text import MIMEText # MIMEText for formatting
-    from email.mime.multipart import MIMEMultipart # MIMEMultipart changing sender
-    from json import load # to open contacts.json to see contacts to be able to send email
-
     recipient_lowered = recipient.lower()
     with open(pass_path, 'r') as email_user_data: # open pass.json to get email password and username
         data = load(email_user_data) # load json

e15088cfca7a5da41397a8ed042ef17f1add17ea
Savagecoder77 <savagecoder77@gmail.com>
2018-07-20 21:30:07 -0700

    fixed and completed rotten tomatoes scraper

--- marvin/webscrape.py
+++ marvin/webscrape.py
@@ -26,25 +26,29 @@ def scrapeYoutube(search_query):
     print('Done!')
 
 def scrapeRottentomatoes(search_query):
-    split = search_query.split(" ")
-    search_query_with_under_scores = ("_").join(split)
+    spliting = search_query.split(" ")[0:]
+    search_query_with_under_scores = ("_").join(spliting)
     url = ('https://www.rottentomatoes.com/m/' + search_query_with_under_scores)# combine url with search query from command
     r = requests.get(url) # request page
     page = r.text
     soup = bs(page, 'html.parser') # parse html
     vids = soup.findAll('span', attrs={'class':'meter-value superPageFontColor'}) # search for class meter-value superPageFontColor in html from page
-    raiting = vids[0].getText()
-    essentials.speak('Rotten Tomatoes gave '+ search_query + ' ' + raiting)
-    people_score = soup.findAll('span', attrs={'class':'superPageFontColor', 'style':'vertical-align:top'})
-    score = people_score[0].getText()
-    want_or_like = soup.findAll('div', attrs={'class':'smaller bold hidden-xs superPageFontColor'})
-    like_or_want = want_or_like[0].getText()
-    pg_up = soup.findAll('li', attrs={'class':'meta-row clearfix'})
-    up_pg = pg_up[0].getText()
-    essentials.speak(search_query + ' got a IMDb ' + up_pg)
-    if like_or_want == 'liked it':
-        essentials.speak(score + ' of people liked ' + search_query)
-    elif like_or_want == 'want to see':
-        essentials.speak(score + ' want to see ' + search_query)
-    else:
-        print('Error\nI web scraped the wrong data or Rotten Tomatoes changed their format please report this issue immediatly so we can fix it')
+    try:
+        if vids == []: raise Exception
+        raiting = vids[0].getText()
+        essentials.speak('Rotten Tomatoes gave '+ search_query + ' ' + raiting)
+        people_score = soup.findAll('span', attrs={'class':'superPageFontColor', 'style':'vertical-align:top'})
+        score = people_score[0].getText()
+        want_or_like = soup.findAll('div', attrs={'class':'smaller bold hidden-xs superPageFontColor'})
+        like_or_want = want_or_like[0].getText()
+        pg_up = soup.findAll('li', attrs={'class':'meta-row clearfix'})
+        up_pg = pg_up[0].getText()
+        essentials.speak(search_query + ' got a IMDb ' + up_pg)
+        if like_or_want == 'liked it':
+            essentials.speak(score + ' of people liked ' + search_query)
+        elif like_or_want == 'want to see':
+            essentials.speak(score + ' want to see ' + search_query)
+        else:
+            print('Error\nI web scraped the wrong data or Rotten Tomatoes changed their format please report this issue immediatly so we can fix it')
+    except Exception as e:
+        essentials.speak('I ran into a problem\nThe name of the movie was probably input incorrectly')

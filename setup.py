fbf73eec8e3be011864ca92b688801ecfc909b82
Savagecoder77 <savagecoder77@gmail.com>
2018-07-28 11:57:57 -0700

    fixing marvin_run.sh

--- setup.py
+++ setup.py
@@ -14,6 +14,26 @@ while True:
         pass_new = sha512(new_pass + 'NQZVA').hexdigest()
         break
 
+def unix_linux():
+    path = os.getcwd()
+    marvin_script_path = (path + '/Marvin_Script.py')
+    marvin_env_path = (path + '/marvin-env/bin/activate')
+    os.system('chmod 755 .installs.sh')
+    os.system('./.installs.sh')
+    env = ('source ' + marvin_env_path)
+    script = ('python2.7 ' + marvin_script_path)
+    cd = ('cd ' + path)
+    out2 = open('marvin_run.sh', 'a')
+    out2.write('\n')
+    out2.write(env)
+    out2.write('\n')
+    out2.write(cd)
+    out2.write('\n')
+    out2.write(script)
+    out2.write('\n')
+    out2.write('deactivate')
+    out2.close()
+
 check_os = system()
 os_release = release()
 print ('Decting Operating System')
@@ -34,7 +54,7 @@ print('Please type email password')
 email_pass = raw_input('>')
 
 try:
-    with open('Os.json', 'w') as outfile:
+    with open('.Os.json', 'w') as outfile:
         var2 = {"Marvin_Release":"0.0.2","Os_data":{"OS":check_os,"os_release":os_release}}
         dump(var2, outfile)
     with open(contacts_path, 'w') as outfile1:
@@ -50,8 +70,6 @@ print('Start installs')
 os.system('pip install virtualenv')
 os.system('virtualenv marvin-env')
 
-path = os.getcwd()
-
 if check_os == 'Linux':
     os.system('chmod 755 marvin_run.sh')
     alias = 'alias marvin="' + path + '/marvin_run.sh"'
@@ -59,41 +77,37 @@ if check_os == 'Linux':
     bashrc = os.path.abspath('%s/.bashrc' % homefolder)
     with open(bashrc, 'r') as f:
         lines = f.readlines()
-        if alias not in lines:
+        if alias in lines:
+            print('Please delete your alias command marvin in your .bashrc file')
+            exit()
+        else:
             out = open(bashrc, 'a')
             out.write(alias)
             out.close()
-        else:
-            print('Please delete your alias command marvin in your .bashrc file')
-            exit()
     os.system('source ' + bashrc)
     print('\nGoing to install tkinter for GUI')
     os.system('sudo apt-get install python-tk')
     os.system('cp /usr/lib/python2.7/dist-packages/tk* marvin-env/lib/python2.7/site-packages/')
-    os.system('chmod 755 installs.sh')
-    os.system('./installs.sh')
 
 elif check_os == 'Darwin':
     os.system('chmod 755 marvin_run.sh')
-    alias = 'alias marvin="' + path + '/marvin_run.sh"'
+    alias = ('alias marvin="' + path + '/marvin_run.sh"')
     homefolder = os.path.expanduser('~')
     bashrc = os.path.abspath('%s/.bash_profile' % homefolder)
     with open(bashrc, 'r') as f:
         lines = f.readlines()
-        if alias not in lines:
+        if alias in lines:
+            print('Please delete your alias command marvin in your .bashrc file')
+            exit()
+        else:
             out = open(bashrc, 'a')
             out.write(alias)
             out.close()
-        else:
-            print('Please delete your alias command marvin in your .bashrc file')
-            exit()
     os.system('source ' + bashrc)
     print('We need to install Homebrew so that we can install portaudio')
     os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
     print('Installing portaudio')
     os.system('brew install portaudio')
-    os.system('chmod 755 installs.sh')
-    os.system('./installs.sh')
 
 elif check_os == 'Windows': pass
 else:

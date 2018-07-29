605433b5a84803b73c8c33dd2bab81c1aa91cf55
Savagecoder77 <savagecoder77@gmail.com>
2018-07-28 17:01:38 -0700

    comment on every line to make easer to understand

--- marvin/essentials.py
+++ marvin/essentials.py
@@ -1,53 +1,51 @@
 # Imports
-from smtplib import SMTP # smtplib for connection and sending of email
-from email.mime.text import MIMEText # MIMEText for formatting
-from email.mime.multipart import MIMEMultipart # MIMEMultipart changing sender
 from json import load # to open contacts.json to see contacts to be able to send email
 from gtts import gTTS # gtts for text to speech
+from smtplib import SMTP # smtplib for connection and sending of email
 from subprocess import Popen, PIPE # subprocess for playing audio
+from email.mime.text import MIMEText # MIMEText for formatting
+from email.mime.multipart import MIMEMultipart # MIMEMultipart changing sender
 from speech_recognition import Recognizer, Microphone, UnknownValueError, RequestError # speech_recognition to turn speech to string
 
 ################################
 # File for essential functions #
 ################################
 
-class MarvinEssentials(Exception): pass
+class MarvinEssentials(Exception): pass # class for breaking loops in Marvin_Script.py
 def speak(spokenString):
-    print(spokenString)
+    print(spokenString) # string to speak
     tts = gTTS(text = spokenString, lang = 'en-uk') # create string into mp3 file using gtts
-    tts.save('Speak.mp3')
-    proc = Popen(['mpg321 Speak.mp3'], stdout = PIPE, stderr = PIPE, shell = True)
+    tts.save('Speak.mp3') # save gtts audio as Speak.mp3
+    proc = Popen(['mpg321 Speak.mp3'], stdout = PIPE, stderr = PIPE, shell = True) # Popen command with terminal command arguments
     (out, err) = proc.communicate() # opening speak file
 
 def listen():
-    r = Recognizer()
+    r = Recognizer() # less writing
     with Microphone() as source: # using microphone to detect audio
         r.adjust_for_ambient_noise(source, duration = 0.5) #adjust for ambiet sounds for 1 second
         audio = r.listen(source) # listen for audio
-    data = ''
-    try:
+    data = '' # set data ad nothing
+    try: # in case of errors
         data = r.recognize_google(audio) # recognize with google's speech recognition
-        print('You said: ' + data)
-    except UnknownValueError:
+        print('You said: ' + data) # write what was heard
+    except UnknownValueError: # unknown audio
         print('I didn\'t get that') # when google api didn't understand audio
-        data = 'None'
-    except RequestError as e:
+        data = 'None' # return none
+    except RequestError as e: # request error
         print('Api or connection is not working.\n The error is {0}'.format(e)) # when connection or Api offline
-        data = 'Broken'
-    return data
+        data = 'Broken' # return broken
+    return data # return recognized audio as string
 
 def commandInput(type_of_input):
-    if type_of_input == 1:
-        input_to_return = listen()
-        return input_to_return
-    if type_of_input == 0:
-        input_to_return = raw_input('')
-        return input_to_return
-    else:
-        print('Fatal Error create Issue on Github')
+    if type_of_input == 1: # 1 for listening
+        input_to_return = listen() # listen
+        return input_to_return # return recognized audio as string
+    if type_of_input == 0: # 0 for type input
+        input_to_return = raw_input('') # get input
+        return input_to_return # return text input
 
 def email(recipient, subject, email_message, pass_path, contact_path):
-    recipient_lowered = recipient.lower()
+    recipient_lowered = recipient.lower() # lower recipent for matching in contacts
     with open(pass_path, 'r') as email_user_data: # open pass.json to get email password and username
         data = load(email_user_data) # load json
         email_user = data['email_address'] # get email address
@@ -76,4 +74,4 @@ def email(recipient, subject, email_message, pass_path, contact_path):
         smtp_server.quit() # quit connection
         print('Email Sent!') # done
     else: # recipient not in contacts
-        speak(recipient + ' is not in our contacts use the "add contacts" command to add them')
+        speak(recipient + ' is not in our contacts use the "add contacts" command to add them') # user not found message

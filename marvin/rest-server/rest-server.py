ed6e68d78159df9d85d0cc5f718057155db6313f
Savagecoder77 <savagecoder77@gmail.com>
2018-07-31 20:46:00 -0700

    added 404 response

--- marvin/rest-server/rest-server.py
+++ marvin/rest-server/rest-server.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,6 +14,5 @@ def home():
 def hello():
     return "Hello World!"
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7070, debug=False)
+    app.run(host='0.0.0.0', port=7070, debug=False, threaded=True)

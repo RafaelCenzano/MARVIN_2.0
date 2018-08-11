68289bd2c6a1329e6e60f4fc8d3916d435e6458e
Savagecoder77 <savagecoder77@gmail.com>
2018-08-11 09:52:14 -0700

    created files to add marvin version to os.json

---
+++ marvin/create_files.py
@@ -0,0 +1,18 @@
+import webscrape
+from platform import system, release
+import json
+import os
+contacts_path = os.path.join('marvin','json','contacts.json')
+
+check_os = system()
+os_release = release()
+marvin_version = webscrape.getVersion()
+try:
+    with open('Os.json', 'w') as outfile:
+        var2 = {"Marvin_Release":marvin_version,"Os_data":{"OS":check_os,"os_release":os_release},"apps":{"IOS":"INACTIVE"}}
+        json.dump(var2, outfile)
+    with open(contacts_path, 'w') as outfile1:
+        var = {"contacts":{},"nicks":{}}
+        json.dump(var, outfile1)
+except Exception as e:
+    print('We ran into a problem\nPlease report this issue ' + str(e) + '\nFiles couldn\'t be created properly')
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /marvin-env/bin/activate
+marvin-env/bin/python2.7 marvin/create_files.py

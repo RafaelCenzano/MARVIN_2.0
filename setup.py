3220104f076033343c1abd9874f87d7026bc3884
Savagecoder77 <savagecoder77@gmail.com>
2018-08-30 21:15:55 -0700

    optimized for python3 needs work on finding python3 version

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ import os
 from platform import system, release
 from json import dump, load
 from hashlib import sha512
+import sys
 
 # Main Code
 while True:
     print('Setting Up Marvin\nADMIN acount username is ADMIN\nPlease set a password for the ADMIN acount\nMake the password longer than 5 characters')
-    new_pass = raw_input('>')
+    new_pass = input('>')
     if len(new_pass) <= 5:
         print('Make your password longer than 5 characters please')
     else:
-        pass_new = sha512(new_pass + 'NQZVA').hexdigest()
+        pass_new = sha512(new_pass.encode('utf-8') + 'NQZVA'.encode('utf-8')).hexdigest()
         break
 
 path = os.getcwd()
@@ -60,6 +61,7 @@ def unix_linux(pass_path, python_path, os):
     with open(pass_path, 'w') as outfile2:
         var1 = {"email_address":email_usr, "email_password":email_pass, "logins":{"ADMIN":{"pass":pass_new}}}
         dump(var1, outfile2)
+    os.system(path + '/marvin-env/bin/python marvin/create_files.py')
     print('\n\nAll files and installs completed\nYou can now run Marvin with the command marvin')
 
 check_os = system()
@@ -76,9 +78,9 @@ print('We need your email to be able to send emails')
 print('\n#####\nYou will need to change an email setting that allows less secure apps\n#####\n')
 print('\nIf you dont want the email function type none')
 print('Please type email address')
-email_usr = raw_input('>')
+email_usr = input('>')
 print('Please type email password')
-email_pass = raw_input('>')
+email_pass = input('>')
 
 print('Starting installs')
 
@@ -98,7 +100,20 @@ if check_os == 'Linux':
     os.system('source ' + bashrc)
     print('\nGoing to install tkinter for GUI')
     os.system('sudo apt-get install python-tk')
-    unix_linux(pass_path, 'Linux')
+    if '3.7' in sys.version():
+        unix_linux(pass_path, '/usr/bin/python3.7', 'Linux')
+    elif '3.6' in sys.version():
+        unix_linux(pass_path, '/usr/bin/python3.6', 'Linux')
+    elif '3.5' in sys.version():
+        unix_linux(pass_path, '/usr/bin/python3.5', 'Linux')
+    elif '3.4' in sys.version():
+        unix_linux(pass_path, '/usr/bin/python3.4', 'Linux')
+    elif '3.3' in sys.version():
+        unix_linux(pass_path, '/usr/bin/python3.3', 'Linux')
+    else:
+        print('Error python3 version not found please type full absolute path to your python3')
+        python_path = input('>')
+        unix_linux(pass_path, python_path, 'Linux')
 
 elif check_os == 'Darwin':
     alias = ('alias marvin="' + path + '/marvin_run.sh"')
@@ -124,10 +139,10 @@ elif check_os == 'Darwin':
     print(os.system('which python3.6'))
     print(os.system('which python3.7'))
     print('Type the path to your python3.7 or python3 executable')
-    input_python_path = raw_input('Please paste the path to the highest version of python3 that you have\n>')
-    if(os.path.isfile(input_python_path))
+    input_python_path = input('Please paste the path to the highest version of python3 that you have\n>')
+    if(os.path.isfile(input_python_path)):
         unix_linux(pass_path, input_python_path, 'Darwin')
-    elif(os.path.isfile('/usr/local/bin/python3'))
+    elif(os.path.isfile('/usr/local/bin/python3')):
         unix_linux(pass_path, '/usr/local/bin/python3', 'Darwin')
     else:
         os.system('brew install python')
@@ -137,7 +152,7 @@ elif check_os == 'Windows':
     python_path = os.path.join('C:','\\Python27','python.exe')
     if os.path.isfile(python_path) == False:
         print("Python2.7 not installed or is not in the default location. Please input your path to python.exe in your Python27 folder. \nExample:\nC:\\Documents\\Programming\\Python27\\python.exe\n\nIf you do not have python installed please close the program and install it here: https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi and install it in the default location")
-        python_path = raw_input('>')
+        python_path = input('>')
         if os.path.isfile(python_path) == False:
             print('Please install python2.7 here: https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi')
             exit()

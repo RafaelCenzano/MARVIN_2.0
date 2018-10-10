bb8a54d7c6931b62e3a420240babc3031f77d527
Savagecoder77 <savagecoder77@gmail.com>
2018-10-09 21:01:55 -0700

    create full random taco service

--- marvin/api.py
+++ marvin/api.py
@@ -7,7 +7,7 @@ from marvin.essentials import speak
 #########################
 
 
-class RetrieveApiService():
+class ApiService():
     def __init__(self, speak_type):
         self.speak_type = speak_type
 
@@ -25,35 +25,32 @@ class RetrieveApiService():
         url = 'http://taco-randomizer.herokuapp.com/random/'
         self.requested_taco = get(url)
 
-class ApiDataProcessing():
-    def __init__(self):
-        pass
-    def dataTaco(self, taco_data):
+    def dataTaco(self):
         try:
-            instructions = taco_data.requested_taco.json()['recipe']
-            print('\n\nInstructions:\n' + instructions, file=open(taco_data.taco_txt + '.txt', 'a'))
+            instructions = self.requested_taco.json()['recipe']
+            print('\n\nInstructions:\n' + instructions, file=open(self.taco_txt + '.txt', 'a'))
         except:
-            print('\n\nInstructions:\nNo recipe for this section', file=open(taco_data.taco_txt + '.txt', 'a'))
+            print('\n\nInstructions:\nNo recipe for this section', file=open(self.taco_txt + '.txt', 'a'))
 
-        if taco_data.requested_taco.json()['shell_url'] is not None:
-            shell_instructions = taco_data.requested_taco.json()['shell']['recipe']
-            print('\n\nShell Instructions:\n' + shell_instructions, file=open(taco_data.taco_txt + '.txt', 'a'))
+        if self.requested_taco.json()['shell_url'] is not None:
+            shell_instructions = self.requested_taco.json()['shell']['recipe']
+            print('\n\nShell Instructions:\n' + shell_instructions, file=open(self.taco_txt + '.txt', 'a'))
 
-        if taco_data.requested_taco.json()['base_layer_url'] is not None:
-            base_layer_instructions = taco_data.requested_taco.json()['base_layer']['recipe']
-            print('\n\nBase Layer Instructions:\n' + base_layer_instructions, file=open(taco_data.taco_txt + '.txt', 'a'))
+        if self.requested_taco.json()['base_layer_url'] is not None:
+            base_layer_instructions = self.requested_taco.json()['base_layer']['recipe']
+            print('\n\nBase Layer Instructions:\n' + base_layer_instructions, file=open(self.taco_txt + '.txt', 'a'))
 
-        if taco_data.requested_taco.json()['seasoning_url'] is not None:
-            season_instructions = taco_data.requested_taco.json()['seasoning']['recipe']
-            print('\n\nSeasoning Instructions:\n' + season_instructions, file=open(taco_data.taco_txt + '.txt', 'a'))
+        if self.requested_taco.json()['seasoning_url'] is not None:
+            season_instructions = self.requested_taco.json()['seasoning']['recipe']
+            print('\n\nSeasoning Instructions:\n' + season_instructions, file=open(self.taco_txt + '.txt', 'a'))
 
-        if taco_data.requested_taco.json()['condiment_url'] is not None:
-            condiment_instructions = taco_data.requested_taco.json()['condiment']['recipe']
-            print('\n\nCondiment Instructions:\n' + condiment_instructions, file=open(taco_data.taco_txt + '.txt', 'a'))
+        if self.requested_taco.json()['condiment_url'] is not None:
+            condiment_instructions = self.requested_taco.json()['condiment']['recipe']
+            print('\n\nCondiment Instructions:\n' + condiment_instructions, file=open(self.taco_txt + '.txt', 'a'))
 
-        if taco_data.requested_taco.json()['mixin_url'] is not None:
-            mixin_instructions = taco_data.requested_taco.json()['mixin']['recipe']
-            print('\n\nMixin Instructions:\n' + mixin_instructions, file=open(taco_data.taco_txt + '.txt', 'a'))
+        if self.requested_taco.json()['mixin_url'] is not None:
+            mixin_instructions = self.requested_taco.json()['mixin']['recipe']
+            print('\n\nMixin Instructions:\n' + mixin_instructions, file=open(self.taco_txt + '.txt', 'a'))
 
-        print('Full Recipe put into file called ' + taco_data.taco_txt)
-        speak('Opening the full taco recipe for you', taco_data.speak_type)
+        print('Full Recipe put into file called ' + self.taco_txt)
+        speak('Opening the full taco recipe for you', self.speak_type)
